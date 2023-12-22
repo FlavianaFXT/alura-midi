3585964465ca7c4deae23d2d32ed860ff7f1762b
Flaviana Ferraz <113718720+FlavianaFXT@users.noreply.github.com>
2023-12-22 12:08:27 -0300

    funçoesanonimas

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+function tocaSom (idEletoAudio){
+document.querySelector(idEletoAudio).play();
+}
+
 let contador = 0;
 
 while(contador < listaDeTeclas.length){
-    listaDeTeclas[contador].onclick = tocaSomPom;
+   
+    listaDeTeclas[contador].onclick = function (){
+        tocaSom('#som_tecla_pom');
+    };
+
     contador = contador + 1;
     console.log(contador);
 }
+
 
