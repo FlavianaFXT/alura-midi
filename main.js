5b246ada37fc3fb9dcaafff4772b32df26f679e9
Flaviana Ferraz <113718720+FlavianaFXT@users.noreply.github.com>
2023-12-22 12:36:03 -0300

    repetiçao otimizada com for

--- main.js
+++ main.js
@@ -4,22 +4,33 @@ function tocaSom (idEletoAudio){
 document.querySelector(idEletoAudio).play();
 }
 
-let contador = 0;
+// let contador = 0;
 const tecla = listaDeTeclas[contador]
 const instrumento = tecla.classList[1];
 //TEMPLATE STRING
 const idAudio = `#som_${instrumento}`;
 
+
 // console.log(instrumento)
 
-while(contador < ltecla.length){
+//ENQUANTO
+// while(contador < tecla.length){
    
-   tecla.onclick = function (){
-        tocaSom(idAudio);
-    };
+//    tecla.onclick = function (){
+//         tocaSom(idAudio);
+//     };
 
-    contador = contador + 1;
-    console.log(contador);
-}
+//     contador = contador + 1;
+//     console.log(contador);
+// }
+
+for(let contador=0; contador < tecla.length; contador++){
+   
+    tecla.onclick = function (){
+         tocaSom(idAudio);
+     };
+
+     console.log(contador);
+ }
 
 
